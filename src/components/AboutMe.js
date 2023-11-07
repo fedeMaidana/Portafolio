18e234ea709dc567225969c11d43244bfc41c7c2
Federico Maidana <fede_maidana4@hotmail.com>
2023-11-07 12:08:08 -0300

    Resolviendo problemas de enrutamiento pt. 13

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,4 +1,4 @@
-import { Axios } from "axios"
+// import { Axios } from "axios"
 
 export const AboutMe = async () => {
     try {
@@ -9,7 +9,7 @@ export const AboutMe = async () => {
             })
         }
 
-        const responseAbout = await fetch( './json/aboutMe.json', requestOptions )
+        const responseAbout = await fetch( 'json/aboutMe.json', requestOptions )
         const { info } = await responseAbout.json()
 
         const responseSkills1 = await fetch( '../src/json/skills1.json', requestOptions )
